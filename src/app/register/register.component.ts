b93f26c05c5f5bb4b9c647dd189f20169eebd251
sahil <sahil@gmail.com>
2023-03-02 18:08:39 +0530

    reactive forms login singup

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -8,7 +8,8 @@ import { AuthService } from '../service/auth.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent  {
+  genders = ['Female','Male'];
   constructor(private formbuilder: FormBuilder,   // for creating the Reactive form with controls
               private toastr : ToastrService,     // for the typical notification 
               private auth  : AuthService,
@@ -16,10 +17,10 @@ export class RegisterComponent {
               ) { }
 
   registerForm = this.formbuilder.group({
-    id: this.formbuilder.control('', Validators.compose([Validators.required, Validators.minLength(5)])),
-    name: this.formbuilder.control('', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]')])),
+    id: this.formbuilder.control('', Validators.compose([Validators.required, Validators.minLength(2)])),
+    name: this.formbuilder.control('', Validators.required), //Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]')])
     email: this.formbuilder.control('', Validators.compose([Validators.email, Validators.required])),
-    password: this.formbuilder.control('', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z\d$#@!.&].{8,12}')])),
+    password: this.formbuilder.control('',Validators.required ),  //Validators.compose([Validators.required, Validators.pattern('[a-zA-Z\d$#@!.&].{8,12}')])
     gender: this.formbuilder.control('male'),
     role: this.formbuilder.control(''),
     isActive: this.formbuilder.control(false)
@@ -29,11 +30,13 @@ export class RegisterComponent {
     if(this.registerForm.valid){
             this.auth.registration(this.registerForm.value).subscribe(res=>{
       
-            console.log(res);
+            // console.log(res);
 
             this.toastr.success('Contact Admin For Activation','Registration Successfull!');
             this.router.navigate(['login']);
       });
+      console.log(this.registerForm.value);
+      
             
     }
     else{
